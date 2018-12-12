36c3257e168b77ac31c7360c98955f0143caa2be
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2018-12-11 19:22:58 -0800

    Extract Macros utility functions to Macros::Util.

--- src/mare/compiler/macros.cr
+++ src/mare/compiler/macros.cr
@@ -21,8 +21,8 @@ class Mare::Compiler::Macros < Mare::AST::Visitor
     # groups that we may match and interpret as if they are macros.
     return node unless node.style == " "
     
-    if match_ident?(node, 0, "if")
-      require_terms(node, [
+    if Util.match_ident?(node, 0, "if")
+      Util.require_terms(node, [
         nil,
         "the condition to be satisfied",
         "the body to be conditionally executed,\n" \
@@ -31,56 +31,6 @@ class Mare::Compiler::Macros < Mare::AST::Visitor
       visit_if(node)
     else
       node
-    end
-  end
-  
-  def match_ident?(node : AST::Group, index : Int32, value : String? = nil)
-    child = node.terms[index]?
-    return false unless child.is_a?(AST::Identifier)
-    return false unless value.nil? || value == child.value
-    true
-  end
-  
-  def require_terms(node : AST::Group, term_docs : Array(String?))
-    if node.terms.size > term_docs.size
-      list = [] of String
-      list << "This macro has too many terms:"
-      list << node.pos.show
-      
-      index = -1
-      while (index += 1) < node.terms.size
-        if index < term_docs.size
-          if term_docs[index]
-            list << "- this term is #{term_docs[index]}:"
-            list << node.terms[index].pos.show
-          end
-        else
-          list << "- this is an excessive term:"
-          list << node.terms[index].pos.show
-        end
-      end
-      
-      raise Error.new(list.join("\n"))
-    end
-    
-    if node.terms.size < term_docs.size
-      list = [] of String
-      list << "This macro has too few terms:"
-      list << node.pos.show
-      
-      index = -1
-      while (index += 1) < term_docs.size
-        if index < node.terms.size
-          if term_docs[index]
-            list << "- this term is #{term_docs[index]}:"
-            list << node.terms[index].pos.show
-          end
-        else
-          list << "- expected a term: #{term_docs[index]}"
-        end
-      end
-      
-      raise Error.new(list.join("\n"))
     end
   end
   
