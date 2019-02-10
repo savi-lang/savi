8db9f1a7b949969c24d4bde5b7b222afa5beaa8d
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2019-02-10 09:41:46 -0800

    Update Completeness pass to do branching analysis of initialized fields.

--- spec/compiler/completeness_spec.cr
+++ spec/compiler/completeness_spec.cr
@@ -31,4 +31,68 @@ describe Mare::Compiler::Completeness do
       Mare::Compiler.compile([source], :completeness)
     end
   end
+  
+  it "complains when a field is only conditionally initialized" do
+    source = Mare::Source.new "(example)", <<-SOURCE
+    class Data:
+      prop x U64:
+      new:
+        if True (
+          @x = 2
+        |
+          if False (
+            @x = 3
+          |
+            @init_x
+          )
+        )
+      fun ref init_x:
+        if True (
+          @x = 4
+        |
+          // fail to initialize x in this branch
+        )
+    SOURCE
+    
+    expected = <<-MSG
+    This constructor doesn't initialize all of its fields:
+    from (example):3:
+      new:
+      ^~~
+    
+    - this field didn't get initialized:
+      from (example):2:
+      prop x U64:
+           ^
+    MSG
+    
+    expect_raises Mare::Error, expected do
+      Mare::Compiler.compile([source], :completeness)
+    end
+  end
+  
+  it "allows a field to be initialized in every case of a choice" do
+    source = Mare::Source.new "(example)", <<-SOURCE
+    class Data:
+      prop x U64:
+      new:
+        if True (
+          @x = 2
+        |
+          if False (
+            @x = 3
+          |
+            @init_x
+          )
+        )
+      fun ref init_x:
+        if True (
+          @x = 4
+        |
+          @x = 5
+        )
+    SOURCE
+    
+    Mare::Compiler.compile([source], :completeness)
+  end
 end
--- src/mare/compiler/completeness.cr
+++ src/mare/compiler/completeness.cr
@@ -1,28 +1,21 @@
-class Mare::Compiler::Completeness < Mare::AST::Visitor
+module Mare::Compiler::Completeness
   def self.run(ctx)
     ctx.program.types.each do |t|
       t.functions.each do |f|
-        new(t, f).run if f.has_tag?(:constructor)
+        check_constructor(t, f) if f.has_tag?(:constructor)
       end
     end
   end
   
-  getter decl : Program::Type
-  getter func : Program::Function
-  getter fields : Array(Program::Function)
-  
-  def initialize(@decl, @func)
-    @fields = decl.functions.select(&.has_tag?(:field))
-    @seen_fields = Set(String).new
-  end
-  
-  def run
-    func.body.try(&.accept(self))
+  def self.check_constructor(decl, func)
+    fields = decl.functions.select(&.has_tag?(:field))
+    branch = Branch.new(decl)
+    func.body.try(&.accept(branch))
     
     unseen =
-      @fields
+      fields
         .select(&.body.nil?) # ignore fields with a default initializer value
-        .reject { |f| @seen_fields.includes?(f.ident.value) }
+        .reject { |f| branch.seen_fields.includes?(f.ident.value) }
         .map { |f| {f.ident, "this field didn't get initialized"} }
     
     Error.at func.ident,
@@ -30,45 +23,62 @@ class Mare::Compiler::Completeness < Mare::AST::Visitor
         unless unseen.empty?
   end
   
-  # This visitor never replaces nodes, it just touches them and returns them.
-  def visit(node)
-    touch(node)
-    
-    node
-  end
-  
-  def visit_children?(node : AST::Choice)
-    # We don't visit anything under a choice - for now, we only consider
-    # completeness of fields that are unconditionally assigned (not in choices).
-    # TODO: Complete a branching analysis of completeness.
-    false
-  end
-  
-  def touch(node : AST::FieldWrite)
-    @seen_fields.add(node.value)
-  end
-  
-  def touch(node : AST::FieldRead)
-    # TODO: Raise an error if we haven't written to that field yet.
-  end
-  
-  def touch(node : AST::Relate)
-    lhs = node.lhs
-    rhs = node.rhs
-    # TODO: Handle more general cases than this?
-    if node.op.value == "." && lhs.is_a?(AST::Identifier) && lhs.value == "@"
-      method_name =
-        case rhs
-        when AST::Identifier then rhs.value
-        when AST::Qualify then rhs.term.as(AST::Identifier).value
-        else raise NotImplementedError.new(rhs.to_a)
-        end
-      
-      decl.find_func?(method_name).try(&.body).try(&.accept(self))
+  class Branch < Mare::AST::Visitor
+    getter decl : Program::Type
+    getter seen_fields : Set(String)
+    def initialize(@decl, @seen_fields = Set(String).new)
     end
-  end
-  
-  def touch(node : AST::Node)
-    # Do nothing for all other AST::Nodes.
+    
+    # This visitor never replaces nodes, it just touches them and returns them.
+    def visit(node)
+      touch(node)
+      
+      node
+    end
+    
+    def visit_children?(node : AST::Choice)
+      # We don't visit anything under a choice with this visitor;
+      # we instead spawn a new visitor instance in the touch method below.
+      false
+    end
+    
+    def touch(node : AST::Choice)
+      # Visit the body of each clause with a new instance of this visitor,
+      # and collect the fields that appeared in all child branches.
+      # A field counts as initialized if it is initialized in all branches.
+      seen_fields.concat(
+        node.list.flat_map do |cond, body|
+          Branch.new(decl, seen_fields.dup).tap { |branch| body.accept(branch) }
+        end.map(&.seen_fields).reduce { |accum, fields| accum & fields }
+      )
+    end
+    
+    def touch(node : AST::FieldWrite)
+      seen_fields.add(node.value)
+    end
+    
+    def touch(node : AST::FieldRead)
+      # TODO: Raise an error if we haven't written to that field yet.
+    end
+    
+    def touch(node : AST::Relate)
+      lhs = node.lhs
+      rhs = node.rhs
+      # TODO: Handle more general cases than this?
+      if node.op.value == "." && lhs.is_a?(AST::Identifier) && lhs.value == "@"
+        method_name =
+          case rhs
+          when AST::Identifier then rhs.value
+          when AST::Qualify then rhs.term.as(AST::Identifier).value
+          else raise NotImplementedError.new(rhs.to_a)
+          end
+        
+        decl.find_func?(method_name).try(&.body).try(&.accept(self))
+      end
+    end
+    
+    def touch(node : AST::Node)
+      # Do nothing for all other AST::Nodes.
+    end
   end
 end
