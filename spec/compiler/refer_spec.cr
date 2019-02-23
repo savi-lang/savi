156753355a684b36bb1332ab56a7d19f43c3f600
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2019-02-23 12:26:06 -0800

    Update Refer pass to do consume tracking and branched analysis.

--- spec/compiler/refer_spec.cr
+++ spec/compiler/refer_spec.cr
@@ -1,9 +1,210 @@
 describe Mare::Compiler::Refer do
-  it "complains when a local declared in a choice is used in another branch" do
-    pending "branched analysis of local declarations"
+  it "fails to resolve a local when it was declared in another branch" do
+    source = Mare::Source.new "(example)", <<-SOURCE
+    actor Main:
+      new:
+        if True (
+          x = "example"
+        |
+          x
+        )
+    SOURCE
+    
+    ctx = Mare::Compiler.compile([source], :refer)
+    
+    func = ctx.program.find_func!("Main", "new")
+    x = func
+      .body.not_nil!
+      .terms.first.as(Mare::AST::Group)
+      .terms.first.as(Mare::AST::Choice)
+      .list.last.last.as(Mare::AST::Group)
+      .terms.first
+    
+    func.refer[x].class.should eq Mare::Compiler::Refer::Unresolved
   end
   
-  it "complains when a consumed local is referenced" do
-    pending "branched analysis of local consumption"
+  it "resolves a local declared in all prior branches" do
+    source = Mare::Source.new "(example)", <<-SOURCE
+    actor Main:
+      new:
+        if True (
+          if True (
+            x = "one"
+          |
+            x = "two"
+          )
+        |
+          x = "three"
+        )
+        x
+    SOURCE
+    
+    ctx = Mare::Compiler.compile([source], :refer)
+    
+    func = ctx.program.find_func!("Main", "new")
+    choice_outer = func
+      .body.not_nil!
+      .terms.first.as(Mare::AST::Group)
+      .terms.first.as(Mare::AST::Choice)
+    
+    choice_inner = choice_outer
+      .list[0].last.as(Mare::AST::Group)
+      .terms.first.as(Mare::AST::Group)
+      .terms.first.as(Mare::AST::Choice)
+    
+    x1 = choice_inner
+      .list[0].last.as(Mare::AST::Group)
+      .terms.first.as(Mare::AST::Relate)
+      .lhs.as(Mare::AST::Identifier)
+    
+    x2 = choice_inner
+      .list[1].last.as(Mare::AST::Group)
+      .terms.first.as(Mare::AST::Relate)
+      .lhs.as(Mare::AST::Identifier)
+    
+    x3 = choice_outer
+      .list[1].last.as(Mare::AST::Group)
+      .terms.first.as(Mare::AST::Relate)
+      .lhs.as(Mare::AST::Identifier)
+    
+    x = func
+      .body.not_nil!
+      .terms.last.as(Mare::AST::Identifier)
+    
+    func.refer[x].as(Mare::Compiler::Refer::LocalUnion).list.should eq [
+      func.refer[x1].as(Mare::Compiler::Refer::Local),
+      func.refer[x2].as(Mare::Compiler::Refer::Local),
+      func.refer[x3].as(Mare::Compiler::Refer::Local),
+    ]
+  end
+  
+  it "complains when referencing a local declared in only some branches" do
+    source = Mare::Source.new "(example)", <<-SOURCE
+    actor Main:
+      new:
+        if True (
+          if True (
+            // missing x
+          |
+            x = "two"
+          )
+        |
+          x = "three"
+        )
+        x
+    SOURCE
+    
+    expected = <<-MSG
+    This variable can't be used here; it was assigned a value in some but not all branches:
+    from (example):12:
+        x
+        ^
+    
+    - it was assigned here:
+      from (example):7:
+            x = "two"
+            ^
+    
+    - it was assigned here:
+      from (example):10:
+          x = "three"
+          ^
+    
+    - but there were other possible branches where it wasn't assigned
+    MSG
+    
+    expect_raises Mare::Error, expected do
+      Mare::Compiler.compile([source], :refer)
+    end
+  end
+  
+  it "complains when an already-consumed local is referenced" do
+    source = Mare::Source.new "(example)", <<-SOURCE
+    actor Main:
+      new:
+        x = "example"
+        --x
+        x
+    SOURCE
+    
+    expected = <<-MSG
+    This variable can't be used here; it might already be consumed:
+    from (example):5:
+        x
+        ^
+    
+    - it was consumed here:
+      from (example):4:
+        --x
+        ^~~
+    MSG
+    
+    expect_raises Mare::Error, expected do
+      Mare::Compiler.compile([source], :refer)
+    end
+  end
+  
+  it "complains when an possibly-consumed local is referenced" do
+    source = Mare::Source.new "(example)", <<-SOURCE
+    actor Main:
+      new:
+        x = "example"
+        if True (--x)
+        x
+    SOURCE
+    
+    expected = <<-MSG
+    This variable can't be used here; it might already be consumed:
+    from (example):5:
+        x
+        ^
+    
+    - it was consumed here:
+      from (example):4:
+        if True (--x)
+                 ^~~
+    MSG
+    
+    expect_raises Mare::Error, expected do
+      Mare::Compiler.compile([source], :refer)
+    end
+  end
+  
+  it "complains when a possibly-consumed local from branches is referenced" do
+    source = Mare::Source.new "(example)", <<-SOURCE
+    actor Main:
+      new:
+        if True (
+          if True (
+            x = "one" // no consume
+          |
+            x = "two", --x
+          )
+        |
+          x = "three", --x
+        )
+        x
+    SOURCE
+    
+    expected = <<-MSG
+    This variable can't be used here; it might already be consumed:
+    from (example):12:
+        x
+        ^
+    
+    - it was consumed here:
+      from (example):7:
+            x = "two", --x
+                       ^~~
+    
+    - it was consumed here:
+      from (example):10:
+          x = "three", --x
+                       ^~~
+    MSG
+    
+    expect_raises Mare::Error, expected do
+      Mare::Compiler.compile([source], :refer)
+    end
   end
 end
--- src/mare/compiler/refer.cr
+++ src/mare/compiler/refer.cr
@@ -1,4 +1,4 @@
-class Mare::Compiler::Refer < Mare::AST::Visitor
+class Mare::Compiler::Refer
   alias RID = UInt64
   
   class Unresolved
@@ -35,6 +35,37 @@ class Mare::Compiler::Refer < Mare::AST::Visitor
     end
   end
   
+  class LocalUnion
+    getter list : Array(Local)
+    property incomplete : Bool = false
+    
+    def initialize(@list)
+    end
+    
+    def self.build(list)
+      any_incomplete = false
+      
+      instance = new(list.flat_map do |elem|
+        case elem
+        when Local
+          elem
+        when LocalUnion
+          any_incomplete |= true if elem.incomplete
+          elem.list
+        else raise NotImplementedError.new(elem.inspect)
+        end
+      end)
+      
+      instance.incomplete = any_incomplete
+      
+      instance
+    end
+    
+    def pos
+      list.first.pos
+    end
+  end
+  
   class Decl
     getter defn : Program::Type
     
@@ -67,16 +98,16 @@ class Mare::Compiler::Refer < Mare::AST::Visitor
   end
   
   alias Info =
-    (Unresolved | Self | Local | Field | Decl | DeclAlias)
+    (Unresolved | Self | Local | LocalUnion | Field | Decl | DeclAlias)
+  
+  property param_count = 0
   
   def initialize(@self_decl : Decl, @decls : Hash(String, Decl | DeclAlias))
     @last_rid = 0_u64
-    @last_param = 0
     @rids = {} of RID => Info
-    @current_locals = {} of String => Local
   end
   
-  private def new_rid(info : Info)
+  def new_rid(info : Info)
     rid = (@last_rid += 1)
     raise "refer id overflow" if rid == 0
     @rids[rid] = info
@@ -87,9 +118,18 @@ class Mare::Compiler::Refer < Mare::AST::Visitor
     @rids[node.rid]
   end
   
-  private def decl(name)
+  def []=(node, info)
+    @rids[node.rid] = info
+  end
+  
+  def decl(name)
     return @self_decl if name == "@"
     @decls[name]
+  end
+  
+  def decl?(name)
+    return @self_decl if name == "@"
+    @decls[name]?
   end
   
   def decl_defn(name) : Program::Type
@@ -124,114 +164,214 @@ class Mare::Compiler::Refer < Mare::AST::Visitor
     return if func.refer? # skip if we've already completed this analysis
     func.refer = self
     
-    func.params.try(&.accept(self))
-    func.ret.try(&.accept(self))
-    func.body.try(&.accept(self))
-  end
-  
-  # This visitor never replaces nodes, it just touches them and returns them.
-  def visit(node)
-    touch(node)
+    root = Branch.new(self)
     
-    create_param_local(node) if Classify.param?(node)
+    func.params.try(&.accept(root))
+    func.ret.try(&.accept(root))
+    func.body.try(&.accept(root))
+  end
+  
+  class Branch < Mare::AST::Visitor
+    getter locals
+    getter consumes
     
-    node
-  end
-  
-  # For an Identifier, resolve it to any known local or decl if possible.
-  def touch(node : AST::Identifier)
-    name = node.value
-    
-    # If this is an @ symbol, it refers to the this/self object.
-    info =
-      if name == "@"
-        Self::INSTANCE
-      else
-        # First, try to resolve as local, then try decls, else it's unresolved.
-        @current_locals[name]? || @decls[name]? || Unresolved::INSTANCE
-      end
-    
-    node.rid = new_rid(info)
-  end
-  
-  # For a FieldRead or FieldWrite, take note of it by name.
-  def touch(node : AST::FieldRead | AST::FieldWrite)
-    node.rid = new_rid(Field.new(node.pos, node.value))
-  end
-  
-  # For a Relate, pay attention to any relations that are relevant to us.
-  def touch(node : AST::Relate)
-    if node.op.value == "="
-      create_local(node.lhs) \
-        if node.lhs.rid == 0 || self[node.lhs] == Unresolved::INSTANCE
-    end
-  end
-  
-  def touch(node : AST::Node)
-    # On all other nodes, do nothing.
-  end
-  
-  def create_local(node : AST::Identifier)
-    # This will be a new local, so if the identifier already matched an
-    # existing local, it would shadow that, which we don't currently allow.
-    if @rids[node.rid].is_a?(Local)
-      Error.at node, "This local shadows an existing local", [
-        {@rids[node.rid], "the first definition was here"},
-      ]
+    def initialize(
+      @refer : Refer,
+      @locals = {} of String => (Local | LocalUnion),
+      @consumes = {} of (Local | LocalUnion) => Source::Pos,
+    )
     end
     
-    # Create the local entry, so later references to this name will see it.
-    local = Local.new(node.pos, node.value, node.rid)
-    @current_locals[node.value] = local unless node.value == "_"
-    @rids[node.rid] = local
-  end
-  
-  def create_local(node : AST::Node)
-    raise NotImplementedError.new(node.to_a) \
-      unless node.is_a?(AST::Group) && node.style == " " && node.terms.size == 2
+    def sub_branch(init_locals = @locals.dup)
+      Branch.new(@refer, init_locals)
+    end
     
-    create_local(node.terms[0])
-    node.rid = node.terms[0].rid
-  end
-  
-  def create_param_local(node : AST::Identifier)
-    case self[node]
-    when Unresolved
-      # Treat this as a parameter with only an identifier and no type.
-      ident = node
+    # This visitor never replaces nodes, it just touches them and returns them.
+    def visit(node)
+      touch(node)
       
-      local = Local.new(node.pos, ident.value, ident.rid, @last_param += 1)
-      @current_locals[ident.value] = local unless ident.value == "_"
-      @rids[ident.rid] = local
-    else
-      # Treat this as a parameter with only a type and no identifier.
-      # Do nothing other than increment the parameter count, because
-      # we don't want to overwrite the Decl info for this node's rid.
-      # We don't need to create a Local anyway, because there's no way to
-      # fetch the value of this parameter later (because it has no identifier).
-      @last_param += 1
+      create_param_local(node) if Classify.param?(node)
+      
+      node
     end
-  end
-  
-  def create_param_local(node : AST::Relate)
-    raise NotImplementedError.new(node.to_a) \
-      unless node.is_a?(AST::Relate) && node.op.value == "DEFAULTPARAM"
     
-    create_param_local(node.lhs)
+    # For an Identifier, resolve it to any known local or decl if possible.
+    def touch(node : AST::Identifier)
+      name = node.value
+      
+      # If this is an @ symbol, it refers to the this/self object.
+      info =
+        if name == "@"
+          Self::INSTANCE
+        else
+          # First, try to resolve as local, then try decls, else it's unresolved.
+          @locals[name]? || @refer.decl?(name) || Unresolved::INSTANCE
+        end
+      
+      # Raise an error if trying to use an "incomplete" union of locals.
+      if info.is_a?(LocalUnion) && info.incomplete
+        extra = info.list.map do |local|
+          {local.pos, "it was assigned here"}
+        end
+        extra << {Source::Pos.none,
+          "but there were other possible branches where it wasn't assigned"}
+        
+        Error.at node,
+          "This variable can't be used here;" \
+          " it was assigned a value in some but not all branches", extra
+      end
+      
+      # Raise an error if trying to use a consumed local.
+      if info.is_a?(Local | LocalUnion) && @consumes.has_key?(info)
+        Error.at node,
+          "This variable can't be used here; it might already be consumed", [
+            {@consumes[info], "it was consumed here"}
+          ]
+      end
+      if info.is_a?(LocalUnion) && info.list.any? { |l| @consumes.has_key?(l) }
+        Error.at node,
+          "This variable can't be used here; it might already be consumed",
+          info.list.select { |l| @consumes.has_key?(l) }.map { |local|
+            {@consumes[local], "it was consumed here"}
+          }
+      end
+      
+      node.rid = @refer.new_rid(info)
+    end
     
-    node.rid = node.lhs.rid
-  end
-  
-  def create_param_local(node : AST::Node)
-    raise NotImplementedError.new(node.to_a) \
-      unless node.is_a?(AST::Group) && node.style == " " && node.terms.size == 2
+    def touch(node : AST::Prefix)
+      raise NotImplementedError.new(node.op.value) unless node.op.value == "--"
+      
+      info = @refer[node.term]
+      Error.at node, "Only a local variable can be consumed" \
+        unless info.is_a?(Local | LocalUnion)
+      
+      @consumes[info] = node.pos
+    end
     
-    ident = node.terms[0].as(AST::Identifier)
+    # For a FieldRead or FieldWrite, take note of it by name.
+    def touch(node : AST::FieldRead | AST::FieldWrite)
+      node.rid = @refer.new_rid(Field.new(node.pos, node.value))
+    end
     
-    local = Local.new(node.pos, ident.value, ident.rid, @last_param += 1)
-    @current_locals[ident.value] = local unless ident.value == "_"
-    @rids[ident.rid] = local
+    # For a Relate, pay attention to any relations that are relevant to us.
+    def touch(node : AST::Relate)
+      if node.op.value == "="
+        create_local(node.lhs) \
+          if node.lhs.rid == 0 || @refer[node.lhs] == Unresolved::INSTANCE
+      end
+    end
     
-    node.rid = ident.rid
+    # We don't visit anything under a choice with this visitor;
+    # we instead spawn new visitor instances in the touch method below.
+    def visit_children?(node : AST::Choice)
+      false
+    end
+    
+    # For a Choice, do a branching analysis of the clauses contained within it.
+    def touch(node : AST::Choice)
+      # Prepare to collect the list of new locals exposed in each branch.
+      branch_locals = {} of String => Array(Local | LocalUnion)
+      
+      # Iterate over each clause, visiting both the cond and body of the clause.
+      node.list.each do |cond, body|
+        # Visit the cond first.
+        cond_branch = sub_branch
+        cond.accept(cond_branch)
+        
+        # Absorb any consumes from the cond branch into this parent branch.
+        @consumes.merge!(cond_branch.consumes)
+        
+        # Visit the body next. Locals from the cond are available in the body.
+        body_branch = sub_branch(cond_branch.locals)
+        body.accept(body_branch)
+        
+        # Absorb any consumes from the body branch into this parent branch.
+        @consumes.merge!(body_branch.consumes)
+        
+        # Collect the list of new locals exposed in the body branch.
+        body_branch.locals.each do |name, local|
+          next if @locals[name]?
+          (branch_locals[name] ||= Array(Local | LocalUnion).new) << local
+        end
+      end
+      
+      # Expose the locals from the branches as LocalUnion instances.
+      # Those locals that were exposed in only some of the branches are to be
+      # marked as incomplete, so that we'll see an error if we try to use them.
+      branch_locals.each do |name, list|
+        info = LocalUnion.build(list)
+        info.incomplete = true if list.size < node.list.size
+        @locals[name] = info
+      end
+    end
+    
+    def touch(node : AST::Node)
+      # On all other nodes, do nothing.
+    end
+    
+    def create_local(node : AST::Identifier)
+      # This will be a new local, so if the identifier already matched an
+      # existing local, it would shadow that, which we don't currently allow.
+      if @refer[node].is_a?(Local)
+        Error.at node, "This variable shadows an existing variable", [
+          {@refer[node], "the first definition was here"},
+        ]
+      end
+      
+      # Create the local entry, so later references to this name will see it.
+      local = Local.new(node.pos, node.value, node.rid)
+      @locals[node.value] = local unless node.value == "_"
+      @refer[node] = local
+    end
+    
+    def create_local(node : AST::Node)
+      raise NotImplementedError.new(node.to_a) \
+        unless node.is_a?(AST::Group) && node.style == " " && node.terms.size == 2
+      
+      create_local(node.terms[0])
+      node.rid = node.terms[0].rid
+    end
+    
+    def create_param_local(node : AST::Identifier)
+      case @refer[node]
+      when Unresolved
+        # Treat this as a parameter with only an identifier and no type.
+        ident = node
+        
+        local = Local.new(node.pos, ident.value, ident.rid, @refer.param_count += 1)
+        @locals[ident.value] = local unless ident.value == "_"
+        @refer[ident] = local
+      else
+        # Treat this as a parameter with only a type and no identifier.
+        # Do nothing other than increment the parameter count, because
+        # we don't want to overwrite the Decl info for this node's rid.
+        # We don't need to create a Local anyway, because there's no way to
+        # fetch the value of this parameter later (because it has no identifier).
+        @refer.param_count += 1
+      end
+    end
+    
+    def create_param_local(node : AST::Relate)
+      raise NotImplementedError.new(node.to_a) \
+        unless node.is_a?(AST::Relate) && node.op.value == "DEFAULTPARAM"
+      
+      create_param_local(node.lhs)
+      
+      node.rid = node.lhs.rid
+    end
+    
+    def create_param_local(node : AST::Node)
+      raise NotImplementedError.new(node.to_a) \
+        unless node.is_a?(AST::Group) && node.style == " " && node.terms.size == 2
+      
+      ident = node.terms[0].as(AST::Identifier)
+      
+      local = Local.new(node.pos, ident.value, ident.rid, @refer.param_count += 1)
+      @locals[ident.value] = local unless ident.value == "_"
+      @refer[ident] = local
+      
+      node.rid = ident.rid
+    end
   end
 end
