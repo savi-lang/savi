b2734c6ec3a8189b99986fc6db8949b2f997e3b5
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2019-07-03 17:42:54 -0700

    Minor reorganization of file structure for Mare-language tests.

--- spec/compiler/eval_spec.cr
+++ spec/compiler/eval_spec.cr
@@ -1,26 +1,26 @@
 describe Mare::Compiler::Eval do
   it "evaluates the semantic tests" do
-    source = fixture "semantic_tests.mare"
+    source_dir = File.join(__DIR__, "../mare/semantics")
     
     no_test_failures =
-      Mare::Compiler.compile([source], :eval).eval.exitcode == 0
+      Mare::Compiler.compile(source_dir, :eval).eval.exitcode == 0
     
     no_test_failures.should eq true
   end
   
   it "evaluates the prelude tests" do
-    source = fixture "prelude_tests.mare"
+    source_dir = File.join(__DIR__, "../mare/prelude")
     
     no_test_failures =
-      Mare::Compiler.compile([source], :eval).eval.exitcode == 0
+      Mare::Compiler.compile(source_dir, :eval).eval.exitcode == 0
     
     no_test_failures.should eq true
   end
   
   it "evaluates an example" do
-    example_dir = File.join(__DIR__, "../../example")
+    source_dir = File.join(__DIR__, "../../example")
     
-    Mare::Compiler.compile(example_dir, :eval) \
+    Mare::Compiler.compile(source_dir, :eval) \
       .eval.exitcode.should eq 42
   end
 end
