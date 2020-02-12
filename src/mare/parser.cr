f15dd00a0637bfef08c07bcff98be7361622e755
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2020-02-11 21:30:01 -0800

    Cache tokens in Parser by source path.

--- src/mare/parser.cr
+++ src/mare/parser.cr
@@ -1,9 +1,24 @@
 require "pegmatite"
 
 module Mare::Parser
-  def self.parse(source)
-    tokens = Pegmatite.tokenize(Grammar, source.content)
+  # TODO: Cache AST::Document instead of Array(Pegmatite::Token) as soon as
+  # copy-on-mutate patterns are enforced throughout the codebase.
+  @@cache = {} of String => {Source, Array(Pegmatite::Token)}
 
-    Builder.build(tokens, source)
+  def self.tokenize(source : Source)
+    if (cache_result = @@cache[source.path]?; cache_result)
+      cached_source, cached_tokens = cache_result
+      return cached_tokens if cached_source == source
+    end
+
+    Pegmatite.tokenize(Grammar, source.content)
+
+    .tap do |result|
+      @@cache[source.path] = {source, result}
+    end
+  end
+
+  def self.parse(source : Source)
+    Builder.build(tokenize(source), source)
   end
 end
