08955e5a909b147426d52d541ad5b4fc21ff42bc
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2020-05-15 20:07:17 -0700

    Prevent running the Pass::Analyze twice over the same function.

--- src/mare/compiler/pass/analyze.cr
+++ src/mare/compiler/pass/analyze.cr
@@ -10,6 +10,7 @@ abstract class Mare::Compiler::Pass::Analyze(TypeAnalysis, FuncAnalysis)
   def []?(f_link : Program::Function::Link); @for_func[f_link]? end
 
   def run(ctx : Context, library : Program::Library)
+    # Run for each of the types in the library.
     library.types.each do |t|
       run_for_type(ctx, t, t.make_link(library))
     end
@@ -20,8 +21,14 @@ abstract class Mare::Compiler::Pass::Analyze(TypeAnalysis, FuncAnalysis)
     t : Program::Type,
     t_link : Program::Type::Link
   )
+    # If we already have an analysis completed for the type, return it.
+    already_analysis = @for_func[t_link]?
+    return already_analysis if already_analysis
+
+    # Generate the analysis for the type and save it to our map.
     @for_type[t_link] = t_analysis = analyze_type(ctx, t, t_link)
 
+    # Run for each of the functions in the type.
     t.functions.each do |f|
       run_for_func(ctx, f, f.make_link(t_link), t_analysis)
     end
@@ -33,17 +40,25 @@ abstract class Mare::Compiler::Pass::Analyze(TypeAnalysis, FuncAnalysis)
     f_link : Program::Function::Link,
     optional_t_analysis : TypeAnalysis? = nil
   )
+    # If we already have an analysis completed for the function, return it.
+    already_analysis = @for_func[f_link]?
+    return already_analysis if already_analysis
+
+    # If the caller didn't supply the analysis for the type, we look it up.
     t_analysis = optional_t_analysis || @for_type[f_link.type]
 
-    @for_func[f_link] = f_analysis = analyze_func(ctx, f, f_link, t_analysis)
+    # Generate the analysis for the function and save it to our map.
+    @for_func[f_link] = analyze_func(ctx, f, f_link, t_analysis)
   end
 
+  # Required hook to make the pass create an analysis for the given type.
   abstract def analyze_type(
     ctx : Context,
     t : Program::Type,
     t_link : Program::Type::Link
   ) : TypeAnalysis
 
+  # Required hook to make the pass create an analysis for the given function.
   abstract def analyze_func(
     ctx : Context,
     f : Program::Function,
