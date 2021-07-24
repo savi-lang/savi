54b9b8a9bbd22eca48139a45f1a8c883fbf6354c
Lucas Tolchinsky <tonchis@protonmail.com>
2021-07-24 00:24:05 -0300

    Add assert EXP1 <op> EXP2 macro

--- spec/compiler/macros/assertion_spec.cr
+++ spec/compiler/macros/assertion_spec.cr
@@ -1,5 +1,5 @@
 describe Savi::Compiler::Macros do
-  describe "assert" do
+  describe "assert EXPR" do
     it "is transformed into Assert.condition" do
       source = Savi::Source.new_example <<-SOURCE
       :actor Main
@@ -19,6 +19,40 @@ describe Savi::Compiler::Macros do
           [:ident, "True"],
         ]
       ]]
+    end
+  end
+
+  describe "assert EXP1 <op> EXP2" do
+    it "is transformed into Assert.relation" do
+      source = Savi::Source.new_example <<-SOURCE
+      :actor Main
+        :new
+          foo = "foo"
+          assert SideEffects.call != foo
+      SOURCE
+
+      ctx = Savi.compiler.compile([source], :macros)
+      ctx.errors.should be_empty
+
+      func = ctx.namespace.find_func!(ctx, source, "Main", "new")
+      func.body.not_nil!.terms[1].to_a.should eq [:group, "(",
+        [:relate,
+          [:ident, "hygienic_macros_local.1"],
+          [:op, "="],
+          [:call, [:ident, "SideEffects"], [:ident, "call"]],
+        ],
+        [:call,
+          [:ident, "Assert"],
+          [:ident, "relation"],
+          [:group, "(", 
+            [:ident, "@"],
+            [:string, "!=", nil],
+            [:ident, "hygienic_macros_local.1"],
+            [:ident, "foo"],
+            [:relate, [:ident, "hygienic_macros_local.1"], [:op, "!="], [:ident, "foo"]]
+          ]
+        ]
+      ]
     end
   end
 end
