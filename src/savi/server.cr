5914db813354406eb9b68ac2b20648741a0bb348
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2021-08-17 21:26:54 -0700

    Add support for OnTypeFormatting in the LSP server.

--- src/savi/server.cr
+++ src/savi/server.cr
@@ -67,6 +67,10 @@ class Savi::Server
       msg.result.capabilities.completion_provider =
         LSP::Data::ServerCapabilities::CompletionOptions.new(false, [":"])
       msg.result.capabilities.document_formatting_provider = true
+      msg.result.capabilities.document_on_type_formatting_provider =
+        LSP::Data::ServerCapabilities::DocumentOnTypeFormattingOptions.new(
+          "\n", ")", "]"
+        )
       msg
     end
   end
@@ -161,6 +165,38 @@ class Savi::Server
   end
 
   def handle(req : LSP::Message::Formatting)
+    filename = req.params.text_document.uri.path.not_nil!
+    dirname = File.dirname(filename)
+    sources = Savi.compiler.source_service.get_library_sources(dirname)
+
+    ctx = Savi.compiler.compile(sources, :import)
+    doc = ctx.root_docs.find(&.pos.source.path.==(filename)).not_nil!
+
+    edits = AST::Format.run(ctx, ctx.root_library_link, [doc]).flat_map(&.last)
+
+    @wire.respond(req) { |msg|
+      msg.result = edits.map { |edit|
+        LSP::Data::TextEdit.new(
+          LSP::Data::Range.new(
+            LSP::Data::Position.new(
+              edit.pos.row.to_i64,
+              edit.pos.col.to_i64,
+            ),
+            LSP::Data::Position.new(
+              edit.pos.row.to_i64,
+              edit.pos.col.to_i64 + (
+                edit.pos.finish - edit.pos.start
+              ),
+            ),
+          ),
+          new_text: edit.replacement,
+        )
+      }
+      msg
+    }
+  end
+
+  def handle(req : LSP::Message::OnTypeFormatting)
     filename = req.params.text_document.uri.path.not_nil!
     dirname = File.dirname(filename)
     sources = Savi.compiler.source_service.get_library_sources(dirname)
