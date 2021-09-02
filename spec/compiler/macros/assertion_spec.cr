64adb3aaa14ec46b597d68f3c4c51b2f2e760c0a
Lucas Tolchinsky <tonchis@protonmail.com>
2021-09-02 17:17:39 -0300

    Compile `<:` and `!<:` into Assert.type_relation

--- spec/compiler/macros/assertion_spec.cr
+++ spec/compiler/macros/assertion_spec.cr
@@ -67,5 +67,74 @@ describe Savi::Compiler::Macros do
         ]
       ]
     end
+
+    it "is transformed into Assert.type_relation for <: and !<:" do
+      source = Savi::Source.new_example <<-SOURCE
+      :actor Main
+        :new
+          assert: "foo" <: String
+          assert: True !<: String
+      SOURCE
+
+      ctx = Savi.compiler.compile([source], :macros)
+      ctx.errors.should be_empty
+
+      func = ctx.namespace.find_func!(ctx, source, "Main", "new")
+      terms = func.body.not_nil!.terms 
+
+      terms[0].to_a.should eq [:group, "(",
+        [:relate,
+          [:ident, "hygienic_macros_local.1"],
+          [:op, "="],
+          [:string, "foo", nil]],
+        [:relate,
+          [:ident, "hygienic_macros_local.2"],
+          [:op, "="],
+          [:relate,
+            [:ident, "hygienic_macros_local.1"],
+            [:op, "<:"],
+            [:ident, "String"],
+          ],
+        ],
+        [:call,
+          [:ident, "Assert"],
+          [:ident, "type_relation"],
+          [:group, "(",
+            [:ident, "@"],
+            [:string, "<:", nil],
+            [:prefix, [:op, "--"], [:ident, "hygienic_macros_local.1"]],
+            [:string, "String", nil],
+            [:ident, "hygienic_macros_local.2"],
+          ]
+        ]
+      ]
+
+      terms[1].to_a.should eq [:group, "(",
+        [:relate,
+          [:ident, "hygienic_macros_local.3"],
+          [:op, "="],
+          [:ident, "True"]],
+        [:relate,
+          [:ident, "hygienic_macros_local.4"],
+          [:op, "="],
+          [:relate,
+            [:ident, "hygienic_macros_local.3"],
+            [:op, "!<:"],
+            [:ident, "String"],
+          ],
+        ],
+        [:call,
+          [:ident, "Assert"],
+          [:ident, "type_relation"],
+          [:group, "(",
+            [:ident, "@"],
+            [:string, "!<:", nil],
+            [:prefix, [:op, "--"], [:ident, "hygienic_macros_local.3"]],
+            [:string, "String", nil],
+            [:ident, "hygienic_macros_local.4"],
+          ]
+        ]
+      ]
+    end
   end
 end
